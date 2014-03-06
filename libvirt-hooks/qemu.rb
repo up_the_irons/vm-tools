f4c82efc058d819fcd303caca75aca791c734176
Garry Dolley <gdolley@arpnetworks.com>
2014-03-06 01:56:30 -0800

    Adjustments

--- libvirt-hooks/qemu.rb
+++ libvirt-hooks/qemu.rb
@@ -5,11 +5,10 @@
 #
 # Create bridge and tap(s) needed for a VM
 
-require 'rubygems'
-require 'nokogiri'
+require 'vm_config'
 
 @guest_name = ARGV[0].to_s
 @phase      = ARGV[1].to_s
 @command    = ARGV[2].to_s
 
-$stderr.puts 'foo'
+@xml = STDIN.read
