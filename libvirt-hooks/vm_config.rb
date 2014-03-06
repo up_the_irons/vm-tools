a17931b5ea424cb157b11f3ddb1565ed1717fd85
Garry Dolley <gdolley@arpnetworks.com>
2014-03-06 01:44:24 -0800

    Get interface target device names

--- libvirt-hooks/vm_config.rb
+++ libvirt-hooks/vm_config.rb
@@ -3,6 +3,19 @@ require 'nokogiri'
 
 class VMConfig
   def initialize(xml)
-    @xml = xml
+    @xml = Nokogiri::XML(xml)
+  end
+
+  def interface_target_device_names
+    names = []
+
+    @xml.css('interface').each do |interface|
+      target = interface.at_css 'target'
+      if target
+        names << target['dev']
+      end
+    end
+
+    names
   end
 end
--- libvirt-hooks/vm_config_spec.rb
+++ libvirt-hooks/vm_config_spec.rb
@@ -1,5 +1,38 @@
 require File.dirname(__FILE__) + '/vm_config'
 
 describe VMConfig do
+  context "initialization" do
+    it "should create Nokogiri::XML object" do
+      xml = "<xml></xml>"
+      expect(Nokogiri).to receive(:XML).with(xml)
+      VMConfig.new(xml)
+    end
+  end
 
+  describe "interface_target_device_names()" do
+    context "with our sample VM config" do
+      before do
+        f = File.dirname(__FILE__) + '/sample.xml'
+        @xml = File.read(f)
+        @vm_config = VMConfig.new(@xml)
+      end
+
+      it "should return tap1-950 and tap2-950" do
+        expect(@vm_config.interface_target_device_names).to \
+          eq(['tap1-950', 'tap2-950'])
+      end
+    end
+
+    context "with no interaces VM config" do
+      before do
+        f = File.dirname(__FILE__) + '/sample-no-interfaces.xml'
+        @xml = File.read(f)
+        @vm_config = VMConfig.new(@xml)
+      end
+
+      it "should return empty array" do
+        expect(@vm_config.interface_target_device_names).to eq([])
+      end
+    end
+  end
 end
