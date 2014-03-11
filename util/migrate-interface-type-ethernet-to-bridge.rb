d3506251ddfa43b2ee248c2746f88cefaae8cfc8
Garry Dolley <gdolley@arpnetworks.com>
2014-03-11 02:38:19 -0700

    Quick & dirty script to change from our old interface block to new one

---
+++ util/migrate-interface-type-ethernet-to-bridge.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+#
+# :Author: Garry Dolley
+# :Date: 03-11-2014
+
+require 'rubygems'
+require 'nokogiri'
+
+XML_FILE_OLD=ARGV[0].to_s
+XML_FILE_NEW=ARGV[1].to_s
+
+def usage
+  puts "./migrate-interface-type-ethernet-to-bridge.rb <file-old> <file-new>"
+  exit 1
+end
+
+usage if XML_FILE_NEW.empty?
+
+f = File.open(XML_FILE_OLD)
+xml = Nokogiri::XML(f)
+xml.css('interface').each do |interface|
+  interface['type'] = 'bridge'
+end
+f.close
+
+File.open("#{XML_FILE_NEW}", "w") do |f|
+  f.print(xml.to_xml)
+end
