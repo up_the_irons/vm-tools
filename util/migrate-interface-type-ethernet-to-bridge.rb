ba9a06e7635303d1f4d9929b6992b66377c946de
Garry Dolley <gdolley@arpnetworks.com>
2014-03-11 18:52:15 -0700

    Put conversion logic into convert!()

--- util/migrate-interface-type-ethernet-to-bridge.rb
+++ util/migrate-interface-type-ethernet-to-bridge.rb
@@ -16,19 +16,25 @@ end
 
 usage if XML_FILE_NEW.empty?
 
+def convert!(xml)
+  xml.css('interface').each do |interface|
+    interface['type'] = 'bridge'
+
+    target = interface.at_css 'target'
+    tap = target['dev']
+    vlan = tap.sub(/^tap\d+-(\d+)(-\d+)?$/, '\1')
+
+    if script = interface.at_css('script')
+      script.remove
+    end
+  end
+
+  xml
+end
+
 f = File.open(XML_FILE_OLD)
 xml = Nokogiri::XML(f)
-xml.css('interface').each do |interface|
-  interface['type'] = 'bridge'
-
-  target = interface.at_css 'target'
-  tap = target['dev']
-  vlan = tap.sub(/^tap\d+-(\d+)(-\d+)?$/, '\1')
-
-  if script = interface.at_css('script')
-    script.remove
-  end
-end
+xml = convert!(xml)
 f.close
 
 File.open("#{XML_FILE_NEW}", "w") do |f|
